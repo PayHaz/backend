ccfcabe5d2e9c01279e943664e44cebc34a96858
Vadim Ermakov <vadim.ermakov-89@mail.ru>
2024-05-04 11:50:52 +0500

    Any changes

--- app/admin.py
+++ app/admin.py
@@ -3,8 +3,20 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User, Product, ProductImage, ProductFeature, Category, City, ProductFavorite, CategoryImage
 
 
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1  # Количество пустых форм для загрузки изображений
+
+
+class ProductFeatureInline(admin.TabularInline):
+    model = ProductFeature
+    extra = 1  # Количество пустых форм для характеристик
+
+
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'description', 'price', 'author')
+    inlines = [ProductImageInline, ProductFeatureInline]
+    list_display = ['name', 'description', 'price', 'status', 'author', 'category', 'created_at', 'updated_at', 'city']
+    search_fields = ['name', 'description']
 
 
 class ProductCity(admin.ModelAdmin):
